81ea649f0e795d8ddd07bb67b7f60e6e6e0b52cb
sequoyah <sequoyah.n.walters@gmail.com>
2022-11-18 13:05:16 -0600

    update config files

--- vins_estimator/src/rosNodeTest.cpp
+++ vins_estimator/src/rosNodeTest.cpp
@@ -1,8 +1,8 @@
 /*******************************************************
  * Copyright (C) 2019, Aerial Robotics Group, Hong Kong University of Science and Technology
- * 
+ *
  * This file is part of VINS.
- * 
+ *
  * Licensed under the GNU General Public License v3.0;
  * you may not use this file except in compliance with the License.
  *
@@ -240,6 +240,18 @@ int main(int argc, char **argv)
 
     readParameters(config_file);
     estimator.setParameter();
+
+    // added by SW
+    // ----------------------------
+    Eigen::Vector3d initP = Eigen::Vector3d(-5, 0, 5);
+    Eigen::Quaterniond initq;
+    initq.x() = 0.0;
+    initq.y() = 0.3826843129510378;
+    initq.z() = 0.0;
+    initq.w() = 0.9238791677601527;
+    Eigen::Matrix3d initR = initq.normalized().toRotationMatrix();
+    estimator.initFirstPose(initP, initR);
+    // ----------------------------
 
 #ifdef EIGEN_DONT_PARALLELIZE
     ROS_DEBUG("EIGEN_DONT_PARALLELIZE");
